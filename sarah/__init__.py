4ae202c191669f9977121528a6206944785e8241
Wang Bo <kingbolanda@live.com>
2019-07-26 15:11:05 +0200

    project initialize, add oppotunity dataset (#1)
    * setup project init
    
    * remove requirements.txt
    
    * import oppotunity, add logger and type hints
    
    * fix error use X to replace x for training data;
    
    * add pipenv files

---
+++ sarah/__init__.py
@@ -0,0 +1,4 @@
+from . import core
+from . import models
+from . import datasets
+from . import utils
--- sarah/datasets/opportunity.py
+++ sarah/datasets/opportunity.py
@@ -12,25 +12,40 @@
 from io import BytesIO
 from pathlib import Path
 from zipfile import ZipFile
+from typing import Union, Tuple
 
 import requests
 import numpy as np
+from loguru import logger
 
 from sarah.utils import train_test_split
 
-def load_data():
-    """Download , extract & Load opportunity dataset."""
+def load_data() -> Union[Tuple[np.ndarray, np.ndarray], Tuple[np.ndarray, np.ndarray]]:
+    """Download, extract & Load opportunity dataset.
+
+    The value will be split into train and test by :meth:`train_test_split`. Use
+        (X_train, y_train), (X_test, y_test) to unpack returned value.
+
+    :return: A tuple contains `X_train` and `y_train`.
+    :return: A tuple contains `X_test` and `y_test`.
+    """
     uri = 'https://archive.ics.uci.edu/ml/machine-learning-databases' + \
     '/00226/OpportunityUCIDataset.zip'
     opportunity_dir = Path.cwd().joinpath('OpportunityUCIDataset')
     if not opportunity_dir.exists():
+        logger.info('Downloading opportunity dataset...')
         response = requests.get(uri)
+        logger.info('Download finished.')
+        logger.info('Unzipping opportunity dataset...')
         zipfile = ZipFile(BytesIO(response.content))
         zipfile.extractall()
+        logger.info('Unzip finished.')
+    logger.info('Loading opportunity dataset...')
     batches = list(opportunity_dir.glob('**/*.dat'))
     opportunity = np.empty(shape=[0, 250])
     for batch in batches:
         opportunity = np.concatenate((opportunity,np.loadtxt(batch, dtype=float)))
     X, y = opportunity[:, :243], opportunity[:, 243:]
+    logger.info('Load finished.')
     return train_test_split(X, y)
 
